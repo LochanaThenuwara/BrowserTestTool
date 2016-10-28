62f531efbcaf71ea8519d2ff6c7917c83fb87669
LochanaThenuwara <kavindilochana@gmail.com>
2016-10-28 10:04:04 +0530

    set browser name

--- first.py
+++ first.py
@@ -1,10 +1,15 @@
 import webbrowser
 
+f = open('features.txt', 'r')
+x = f.readlines()
+print len(x);
+  
 webbrowser.register('firefox', None, webbrowser.BackgroundBrowser("C:/Program Files (x86)/Mozilla Firefox/firefox.exe"))
 webbrowser.register('chrome', None, webbrowser.BackgroundBrowser("C:/Program Files (x86)/Google/Chrome/Application/chrome.exe"))
 webbrowser.register('ie', None, webbrowser.BackgroundBrowser("C:/Program Files (x86)/Internet Explorer/iexplore.exe"))
 
-webbrowser.get('chrome').open('frontend.html')
 webbrowser.get('firefox').open('frontend.html')
-webbrowser.get('ie').open('frontend.html')
+webbrowser.get('chrome').open('frontend.html')
+
+#webbrowser.get('ie').open('frontend.html')
         
